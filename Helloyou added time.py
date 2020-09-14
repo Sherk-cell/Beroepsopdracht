75e195b9a331060015c1968245472823a42762b5
Sherk-cell <70703520+Sherk-cell@users.noreply.github.com>
2020-09-14 11:53:56 +0200

    Add files via upload

---
+++ Helloyou added time.py
@@ -0,0 +1,9 @@
+print("Hello you!, ik ben Byron")
+
+username = input("Wie ben jij?:")
+
+print("Hello " + username)
+import datetime
+x = datetime.datetime.now()
+print ("Dit is nu de tijd")
+print(x)
---
+++ Helloyou.py
@@ -0,0 +1,5 @@
+print("Hello you!, ik ben Byron")
+
+username = input("Wie ben jij?:")
+
+print("Hello " + username)
